138211aa1e933fa316b13faed1582b90085479aa
Coal <y.liang1994@gmail.com>
2025-12-10 21:09:05 -0800

    Initial commit

---
+++ Assets/Scripts/Chunks/Grid/Grid.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    [Header("Grid Settings")]
+    [SerializeField] public int gridSize;
+    [SerializeField] public int gridIndex;
+    [SerializeField] public Vector2 gridPosition;
+}
