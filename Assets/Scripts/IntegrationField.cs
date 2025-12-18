f703fce23734db6fc6ea8f9d37115530173f6ae5
Coal <y.liang1994@gmail.com>
2025-12-17 20:10:48 -0800

    Enqueue() was not working properly, now it works, but needs to be adapted to Unity

--- Assets/Scripts/IntegrationField.cs
+++ Assets/Scripts/IntegrationField.cs
@@ -2,15 +2,12 @@ using UnityEngine;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 
-
-/// <summary>
-/// This is the PriorityQueue implementation for pathfinding solutions. Consideration is made for usage in A*, IntegrationField, and FlowField
-/// Therefore, only Enqueue and Pop operations are implemented/required for now.
-/// </summary>
+// Pre-edit to work with the pathfinding solution, e.g. Debug.Log and Vector objects
+// This version works as a separate C# class
 public class PriorityQueue
 {
-	public (int value, int prio)[] prioqueue;
-	public int _count; // TODO: make this a property later
+    public (int value, int prio)[] prioqueue;
+    public int _count; // TODO: make this a property later
 
     /* 
 	add (int val){
@@ -22,38 +19,99 @@ public class PriorityQueue
 	emptyspotindex = (emptyspotindex-1)/2
 	 */
 
-    public PriorityQueue(int size) 
-	{
+    public static void Main()
+    {
+        Console.WriteLine("Creating PQ");
+        PriorityQueue pq = new(50);
+        pq.toString(0);
+        Console.WriteLine("Adding 10-larger than before elements\n");
+        var data = new List<(int dx, int dz)> { (0, 0) };
+        for (int i = 1; i < 10; i++)
+        {
+            // Access the previous element (data[i-1]) and increment its fields
+            int nx = data[i - 1].dx + 2;
+            int nz = data[i - 1].dz + 2;
+            (int, int) n = (nx, nz);
+            data.Add(n);
+            // for defaulted identifiers
+            // data[i].Item1 = data[i-1].Item1 + 1;
+            // data[i].Item2 = data[i-1].Item2 + 1;
+        }
+
+        for (int i = 0; i < data.Count(); i++)
+        {
+            Console.WriteLine("{0}", data[i].ToString());
+        }
+
+
+
+
+        for (int i = 0; i < data.Count(); i++)
+        {
+            (int, int) curdata = data[i];
+            pq.Enqueue(data[i]);
+        }
+
+        pq.toString(1);
+        var datatwo = (1, 1);
+        pq.Enqueue(datatwo);
+        Console.WriteLine("Adding a low member");
+        pq.toString(1);
+
+    }
+
+    public PriorityQueue(int maxSize)
+    {
         _count = 0;
-		prioqueue = new (int value, int prio)[size];
-	}
+        prioqueue = new (int value, int prio)[maxSize];
+        for (int i = 0; i < maxSize; i++)
+        {
+            prioqueue[i] = (int.MaxValue, -1); // or whatever sentinel makes sense
+        }
+    }
 
     /// <summary>
 	/// Enqueue operation on the PriorityQueue
     /// _count is the currentIndex of the cell we work on
     /// parentIndex is the index for the parent of the child (the thing you're looking to operate on)
     /// </summary>
-    /// <param name="cellData"></param>
-    public void Enqueue((int cellValue, int cellIndex) cellData)
-	{
-		int parentIndex = (_count - 1) / 2;
-		int currentIndex = _count;
+    /// <param name="tuple"></param>
+    public void Enqueue((int value, int index) tuple)
+    {
+        int currentIndex = _count;
+        int parentIndex = (_count - 1) / 2;
 
         if (_count == 0)
-		{
-			prioqueue[_count++] = cellData;
-			return; // LOW TODO: redundant return, research
-		}
-		
-		while (prioqueue[parentIndex].value != 0 && prioqueue[parentIndex].value > cellData.cellValue) // TODO: don't use sentinel values here
-		{
-			prioqueue[currentIndex] = prioqueue[parentIndex];
-            prioqueue[parentIndex] = cellData;
-			currentIndex = (currentIndex - 1) / 2;
+        {
+            prioqueue[_count++] = tuple;
+            return; // LOW TODO: redundant return, research
+        }
+        bool bA = prioqueue[parentIndex].value != 0;
+        bool bB = prioqueue[parentIndex].value > tuple.value;
+
+        while (prioqueue[parentIndex].value != 0 && prioqueue[parentIndex].value > tuple.value) // TODO: don't use sentinel values here
+        {
+            prioqueue[currentIndex] = prioqueue[parentIndex];
+            prioqueue[parentIndex] = tuple;
+            currentIndex = (currentIndex - 1) / 2;
             parentIndex = (currentIndex - 1) / 2;
         }
-		_count++;
-	}
+        prioqueue[currentIndex] = tuple;
+        _count++;
+    }
+
+    public void toString(int displayContents)
+    {
+        Console.WriteLine("Size is {0}\n" + "Max size is {1}\n", _count, prioqueue.Length);
+        if (Convert.ToBoolean(displayContents))
+        {
+            Console.WriteLine("Printing Contents:\n");
+            for (int i = 0; i < _count; i++)
+            {
+                Console.WriteLine("{0}", prioqueue[i]);
+            }
+        }
+    }
 
 }
 
